88e7d62c5253b5d315f71816d030a1a7fcdeaa5c
anneauzier <anne.auziier@outlook.com>
2022-11-09 15:34:45 -0400

    suggestion screen_1

---
+++ Challenge_Apple_Watch Watch App/InterfaceSuggestion.swift
@@ -0,0 +1,47 @@
+//
+//  InterfaceSuggestion.swift
+//  Challenge_Apple_Watch Watch App
+//
+//  Created by Anne Victoria Batista Auzier on 08/11/22.
+//
+
+import SwiftUI
+import WatchKit
+import Foundation
+import UIKit
+
+class InterfaceSuggestion: WKInterfaceController {
+
+//    func performAction(actionStyle: WKAlertActionStyle) {
+//            switch actionStyle {
+//            case .Default:
+//                print("OK")
+//            case .Cancel:
+//                print("Cancel")
+//            case .Destructive:
+//                print("Destructive")
+//            }
+//        }
+//
+//        @IBAction func btnShowAlerts() {
+//            let okAction = WKAlertAction(title: "OK",
+//                style: WKAlertActionStyle.Default) { () -> Void in
+//                    self.performAction(WKAlertActionStyle.Default)
+//            }
+//
+//            let cancelAction = WKAlertAction(title: "Cancel",
+//                style: WKAlertActionStyle.Cancel) { () -> Void in
+//                    self.performAction(WKAlertActionStyle.Cancel)
+//            }
+//
+//            let abortAction = WKAlertAction(title: "Abort",
+//                style: WKAlertActionStyle.Destructive) { () -> Void in
+//                    self.performAction(WKAlertActionStyle.Destructive)
+//            }
+
+//            presentAlertControllerWithTitle("Title",
+//                message: "Message",
+//                preferredStyle: WKAlertControllerStyle.Alert,
+////                actions: [okAction, cancelAction, abortAction])
+//        }
+}
